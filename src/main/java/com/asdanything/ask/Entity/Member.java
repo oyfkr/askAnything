2e053a26be7dc5790bf16fde05e6720d1b41b608
oyfkr <oyfkr@naver.com>
2021-10-01 19:52:14 +0900

    member 엔티티의 조건 추가
    email 값의 unique 조건 추가

--- src/main/java/com/asdanything/ask/Entity/Member.java
+++ src/main/java/com/asdanything/ask/Entity/Member.java
@@ -19,6 +19,7 @@ public class Member {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(unique = true)
     private String email;
 
     private String name;
