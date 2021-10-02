2ff7b5608fea7494c9d287084e9dd0631e33cabe
oyfkr <oyfkr@naver.com>
2021-10-03 01:46:37 +0900

    회원가입 완성
    validation 추가 , memberFormDto 어노테이션 추가 , memberController 수정 , html 에러 처리

--- src/main/java/com/asdanything/ask/controller/MemberController.java
+++ src/main/java/com/asdanything/ask/controller/MemberController.java
@@ -7,8 +7,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,9 +27,20 @@ public class MemberController {
     }
 
     @PostMapping("/members/new")
-    public String memberNew(MemberFormDto memberFormDto, Model model){
-        Member member = Member.createMember(memberFormDto, passwordEncoder);
-        memberService.saveMember(member);
+    public String memberNew(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors()){
+            return "member/memberForm";
+        }
+
+        try{
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+
+        } catch (IllegalStateException e){
+            model.addAttribute("errorMessage",e.getMessage());
+            return "member/memberForm";
+        }
         return "redirect:/";
     }
 }
--- src/main/java/com/asdanything/ask/dto/MemberFormDto.java
+++ src/main/java/com/asdanything/ask/dto/MemberFormDto.java
@@ -2,10 +2,17 @@ package com.asdanything.ask.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class MemberFormDto {
 
+    @NotBlank(message = "이름은 필수 입력값입니다.")
     private String name;
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식으로 입력해 주세요")
     private String email;
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String password;
 }
